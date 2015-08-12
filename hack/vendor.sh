a9bd0a8a52feeedf3d82e63f1157e995a0d3431f
Christy Perez <clnperez@linux.vnet.ibm.com>
2015-08-12 15:12:33 -0500

    Vendor in ppc64le & gccgo fixes
    Pull in  gccgo-buildtags-GidMappingsEnableSetgroups and
    updated  seccomp-ppc64le
    
    Signed-off-by: Christy Perez <clnperez@linux.vnet.ibm.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -44,7 +44,7 @@ clone git github.com/tent/canonical-json-go 96e4ba3a7613a1216cbd1badca4efe382ade
 clone git github.com/agl/ed25519 d2b94fd789ea21d12fac1a4443dd3a3f79cda72c
 
 #clone git github.com/opencontainers/runc v0.0.2 # libcontainer
-clone git github.com/opencontainers/runc a876bc6a0dffd2f2b32b1eb83166093dd66fe8ba https://github.com/clnperez/runc
+clone git github.com/opencontainers/runc 1830fd28fabc20c308b89c01e69ae300c431f985 https://github.com/clnperez/runc
 # libcontainer deps (see src/github.com/docker/libcontainer/update-vendor.sh)
 clone git github.com/coreos/go-systemd v3
 clone git github.com/godbus/dbus v2
